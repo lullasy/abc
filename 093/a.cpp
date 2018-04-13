c106effb1a0a5c257a1f7ea8430bb6d5f5d68565
li_saku <miu@saku.li>
2018-04-13 15:48:46 +0900

    a ac

--- 093/a.cpp
+++ 093/a.cpp
@@ -1,7 +1,23 @@
-#include <stdio>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 int main(void)
 {
+  string input;
+
+  cin >> input;
+
+  sort(input.begin(), input.end());
+  if (input == "abc"){
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
 
   return 0;
 }
