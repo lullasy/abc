b3775f1e35fc6c251cbd980cd62a72b94cefdc9a
li_saku <cid378@gmail.com>
2019-03-05 12:51:25 +0900

    add template (#2)

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <utility>
+#include <numeric>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <array>
+#include <list>
+#include <stack>
+#include <valarray>
+
+using namespace std;
+
+typedef unsigned uint;
+typedef long long Int;
+typedef unsigned long long UInt;
+
+const int INF = 1001001001;
+const Int INFLL = 1001001001001001001LL;
+
+#define show(x) cerr << #x << " = " << (x) << endl;
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
+template<typename T> void pv(T a, T b) { for (T i = a; i != b; ++i) cout << *i << " "; cout << endl; }
+template<typename T> void chmin(T& a, T b) { if (a > b) a = b; }
+template<typename T> void chmax(T& a, T b) { if (a < b) a = b; }
+int in() { int x; scanf("%d", &x); return x; }
+double fin() { double x; scanf("%lf", &x); return x; }
+Int lin() { Int x; scanf("%lld", &x); return x; }
+
+int main() {
+
+  return 0;
+}
